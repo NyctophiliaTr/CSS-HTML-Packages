7cb2d10318d85562926b86eab448ca7c5b9e3d5e
NyctophiliaTr <110660691+NyctophiliaTr@users.noreply.github.com>
2022-10-24 21:14:26 +0300

    CSS-HTML Packages

---
+++ CSS-HTML Packages/-3-information-form-template/personal-website(2)/js/script.js
@@ -0,0 +1,19 @@
+        /* image.src = 'http://i.imgur.com/Sr5QsYO.jpg'; (Rainy City)
+           image.src = 'http://i.imgur.com/x86Mup0.jpg'  (Snowy Railroad)
+           image.src = 'https://i.imgur.com/OuSizUw.jpeg'(Star Wors)
+           image.src = 'https://i.imgur.com/6bhVxtq.jpeg'(Forest Ghost)
+           image.src = 'https://i.imgur.com/rFvq7l3.jpeg'(City Ghost)
+           image.src = 'https://i.imgur.com/cnWmtI8.jpeg'(Skull)
+           image.src = 'https://i.imgur.com/UqGpA9z.jpeg'(Map)
+           image.src = 'https://i.imgur.com/sQIdq1F.jpeg'(Night)
+           image.src = 'https://i.imgur.com/LWs9DLS.jpeg'(Military Field)
+           image.src = 'https://i.imgur.com/oum4xNg.jpeg'(Pink City)
+           image.src = 'https://i.imgur.com/asSvQG9.jpeg' (Loading)
+           image.src = 'https://imgur.com/gallery/MjIr6' (Tram)
+           */
+
+
+
+
+
+           // 'https://imgur.com/gallery/zh0apUP' Girl Photos  
---
+++ CSS-HTML Packages/-7-Personal-Website-Template/src/information-form-template/script.js
@@ -0,0 +1,8 @@
+    var typed = new Typed(".typing", {
+        strings: ["Developer", "Blogger", "Freelancer",],
+        typeSpeed: 100,
+        backSpeed: 60,
+        cursorChar: ' |',
+        smartBackspace: true,
+        loop: true
+    });
